96ed066b2908fea87a31d644ffa37d6ba33f2392
CodePiru <coding.jose@gmail.com>
2022-01-17 13:42:18 -0500

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elements) {
+    for (let i = 0; i < elements.length; i++) {
+        let index = arr.indexOf(elements[i]);
+        if (index < 0) continue;
+        arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line

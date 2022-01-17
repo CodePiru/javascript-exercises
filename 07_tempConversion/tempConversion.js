9b67ef072947d433d8b0896e42a6b072329f6d91
CodePiru <coding.jose@gmail.com>
2022-01-17 16:02:17 -0500

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celsius = Math.round(((farenheit - 32) * 5 / 9) * 10) / 10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let farenheit = Math.round((celsius * 9 / 5 + 32) * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line

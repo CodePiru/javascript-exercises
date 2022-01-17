7dcebb67b35c3a68650e7e1adff0127cd1102408
CodePiru <coding.jose@gmail.com>
2022-01-17 14:36:32 -0500

    Add solution snippet in comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
+    //TOP's solution snippet
+    //if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
     if (typeof(num1) != "number" || typeof(num2) != "number" || num1 < 0 || num2 < 0) {
         return "ERROR"
     }

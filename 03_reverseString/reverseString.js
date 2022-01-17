9a0b76145edfd54fc254117763fbaa8400b32645
CodePiru <coding.jose@gmail.com>
2022-01-17 12:24:41 -0500

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+    let reversed = "";
+    if (str.length == 0) return reversed;
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line

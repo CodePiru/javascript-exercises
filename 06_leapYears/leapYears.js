d5b71d3f1e2ccef21e30d791311f658666480113
CodePiru <coding.jose@gmail.com>
2022-01-17 15:29:59 -0500

    Add solution in comment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,5 +3,11 @@ const leapYears = function(year) {
     return true;
 };
 
+/*TOP's solution
+const leapYears = function(year) {
+  return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0)
+};
+*/
+
 // Do not edit below this line
 module.exports = leapYears;
